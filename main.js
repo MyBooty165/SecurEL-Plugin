198ac4312cf88430460e8fa54f7da9218146c350
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-03-26 13:26:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const redirectDomains = {
 };
 const emailInput = document.querySelector('input[type="email"]');
 const submitButton = document.querySelector('input[type="submit"]');
+const messageElement = document.querySelector('#message');
 
 emailInput.addEventListener('input', (event) => {
   const emailDomain = event.target.value.split('@')[1];
@@ -11,7 +12,16 @@ emailInput.addEventListener('input', (event) => {
     submitButton.disabled = true;
     window.location.href = 'blockpage.html';
   } else if (redirectDomains[emailDomain]) {
-    window.location.href = redirectDomains[emailDomain];
+    messageElement.textContent = 'Access Granted!';
+    messageElement.style.color = 'green';
+    setTimeout(() => {
+      messageElement.textContent = 'Redirecting to Tam´s Game Hub';
+      messageElement.style.color = 'black';
+      messageElement.style.fontWeight = 'bold';
+      setTimeout(() => {
+        window.location.href = redirectDomains[emailDomain];
+      }, 3000);
+    }, 1000);
   } else {
     submitButton.disabled = false;
   }
